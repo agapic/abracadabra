2691cf94ab0c672695bb5128ed5df6d84a03a970
agapic <andrewjdg@gmail.com>
2016-01-21 22:18:26 -0500

    add file for folder push

---
+++ app/controllers/app.js
@@ -0,0 +1,41 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var hbs = require('hbs');
+var routes = require('./routes/routes');
+
+//Basic config for ports and views
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view options', {layout: 'layout'});
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+//Routes
+app.get('/', routes.index);
+app.get('/champions', routes.champions);
+app.get('/items/', routes.items);
+app.get('/items/:item', routes.itemData);
+app.get('/items/:item/:gamemode', routes.itemData);
+app.get('/items/:item/:gamemode/:region', routes.itemData);
+app.get('/items/:item/:gamemode/:region#lol', routes.itemData);
+app.get('/champions/:gamemode/:region', routes.damage);
+app.get('/champions/:gamemode', routes.damage);
+app.get('*', routes.index);
+
+app.use(errorHandler);
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.send("Error, no such page! Please politely, and kindly go back. :)");
+}
+//Listen to dat port
+app.listen(app.get('port'), function(){
+    console.log("The application is listening on port " + app.get('port'))
+});
+    
+    
