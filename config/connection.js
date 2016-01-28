4dd78c81667acbee13fa98a2a67f007414ee312c
= <andrewjdg@gmail.com>
2016-01-28 12:45:20 -0500

    major front page revamp

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,3 @@
 var connectionString = process.env.DATABASE_URL ||
-    'postgres://postgres:$31=$1@159.203.43.207:5432/abracadabra';
+    'postgres://postgres:$rgx!idxX@159.203.43.207:5432/abracadabra';
 exports.connection = connectionString;
